62042f9486cf75e61ba26c580a36064d52dbb566
Lucian <lucian.miller185@gmail.com>
2021-06-01 10:52:33 -0700

    It will return leap year as false

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     const userYear = parseInt($("#year").val());
+
+    isLeapYear(1993)
+    function isLeapYear(year){
+      return false;
+    }
   });
 });
