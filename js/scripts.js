c6baf51659710590229d06e0515250ce6999297c
Lucian <lucian.miller185@gmail.com>
2021-06-02 10:28:01 -0700

    It will return true for years that are divisible by 400

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     isLeapYear();
     function isLeapYear(year) {
-      if ((year % 4 ===0) && (year % 100 !== 0)) {
+      if ((year % 4 ===0) && (year % 100 !== 0) || (year % 400 === 0)) {
         return true;
       } else {
         return false;
