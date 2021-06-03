d31596d2a235001689af361bc165668682a18a1d
Lucian <lucian.miller185@gmail.com>
2021-06-03 09:38:32 -0700

    It will display to user if the year entered is or isn't a leap year

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
     isLeapYear(userYear);
     function isLeapYear(year) {
       if ((year % 4 ===0) && (year % 100 !== 0) || (year % 400 === 0)) {
-        return true;
+        $(".year").text(userYear);
       } else {
-        return false;
+        $(".year").text(userYear);
+        $(".not").text(" not");
       }
-    };
+    }
+    $("#result").show();
   });
 });
